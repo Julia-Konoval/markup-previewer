8998a3dba2b81066ca573e614a098ce33e889a87
Julia <yulyakonoval1998@gmail.com>
2021-01-21 21:11:07 +0200

    input & output containers designed, media query applied

--- src/Input.js
+++ src/Input.js
@@ -11,26 +11,36 @@ const Input = () => {
   }
   return (
     <div>
-      <div className="input-area">
-        <h1>Type code to preview</h1>
-        <textarea
-          className="input"
-          value={markdown}
-          onChange={(e) => {
-            updateMarkdown(e.target.value);
-          }}
-        >
-          {" "}
-          {console.log(markdown)}
-        </textarea>
+      <div className="header">
+        <h1 className="header-main">Markdown</h1>{" "}
+        <p className="header-p">previewer</p>
       </div>
-      <div className="preview-container">
-        {" "}
-        <h1>Markdown Output</h1>
-        <div
-          className="preview"
-          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-        ></div>
+      <div className="wrapper">
+        <div className="input-area">
+          <h1 className="title">Type code to preview</h1>
+          <textarea
+            className="input"
+            placeholder="# H1     
+          ## H2     
+          ### H3"
+            value={markdown}
+            onChange={(e) => {
+              updateMarkdown(e.target.value);
+            }}
+          >
+            {" "}
+            {console.log(markdown)}
+          </textarea>
+        </div>
+
+        <div className="preview-container">
+          {" "}
+          <h1 className="title">Markdown Output</h1>
+          <div
+            className="preview"
+            dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+          ></div>
+        </div>
       </div>
     </div>
   );
